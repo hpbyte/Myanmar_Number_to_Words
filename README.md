5c8b4b3b6ea2d1b4cb506768452c9f6f4af57d30
Htoo Pyae <htoopyae5@gmail.com>
2019-08-06 16:41:06 +0630

    digit number conversion done

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # mm_num2word
+
+- Basically, this is a simple python script for converting Myanmar numbers into Myanmar spoken words
+- It is expected to be able to detect and convert the following types (Currently only digits):
+  - Dates
+  - Time
+  - Digit Number
+  - Cardinal Number
+  - Range
+  - Fraction
+  - NRC
+  - Money
+
+```
+# Digits
+၁၀၇         =>  တစ်ရာ့ ခုနှစ်
+၁၂၀         =>  တစ်ရာ့ နှစ်ဆယ်
+၃၂၁         =>  သုံ:ရာ နှစ်ဆယ့် တစ်
+၃၂၅၉၇၀၀၀၃၀  =>  သုံ:သောင်: နှစ်ထောင့် ငါ:ရာ ကို:ဆယ့် ခုနှစ်သိန်: သုံ:ဆယ်
+၅၀၀၅၁၂၃၀    =>  သိန်: ငါ:ရာ ငါ:သောင်: တစ်ထောင့် နှစ်ရာ့ သုံ:ဆယ်
+```
---
+++ mm_num2word.py
@@ -0,0 +1,70 @@
+import re
+
+def convert_digit(num):
+  """
+  For Digit Numbers
+
+  @type     num   str
+  @param    num   Myanmar digit number
+  @rtype          str
+  @return         converted Myanmar spoken words
+  """
+
+  mm_digit = {
+    '၁': 'တစ်',
+    '၂': 'နှစ်',
+    '၃': 'သုံ:',
+    '၄': 'လေ:',
+    '၅': 'ငါ:',
+    '၆': 'ခြောက်',
+    '၇': 'ခုနှစ်',
+    '၈': 'ရှစ်',
+    '၉': 'ကို:'
+  }
+
+  converted = ''
+  nb_digits = len(num)
+
+  def check_if_zero(pos):
+    return not num[-pos] == '၀'
+
+  def hundred_thousandth_val():
+    n = num[:-5]
+    return ('သိန်: ' + mm_num2word(n)) if (n[-2:] == '၀၀') else (mm_num2word(n) + 'သိန်: ')
+
+  def thousandth_val():
+    return mm_digit[num[-4]] + ('ထောင် ' if (num[-3:] == '၀၀၀') else 'ထောင့် ')
+
+  def hundredth_val():
+    return mm_digit[num[-3]] + ('ရာ့ ' if ((not re.match(r"[၁-၉]", num[-2:]))) else 'ရာ ')
+
+  def tenth_val():
+    return ('' if (num[-2] == '၁') else mm_digit[num[-2]]) + ('ဆယ် ' if (num[-1] == '၀') else 'ဆယ့် ')
+
+  if ((nb_digits > 5)):
+    converted += hundred_thousandth_val()
+  if ((nb_digits > 4) and check_if_zero(5)):
+    converted += mm_digit[num[-5]] + 'သောင်: '
+  if ((nb_digits > 3) and check_if_zero(4)):
+    converted += thousandth_val()
+  if ((nb_digits > 2) and check_if_zero(3)):
+    converted += hundredth_val()
+  if ((nb_digits > 1) and check_if_zero(2)):
+    converted += tenth_val()
+  if ((nb_digits > 0) and check_if_zero(1)):
+    converted += mm_digit[num[-1]]
+
+  return converted
+
+def mm_num2word(num):
+  """
+  @type     num   str
+  @param    num   Myanmar number
+  @rtype          str
+  @return         converted Myanmar spoken words
+  """
+
+  # digits
+  word = convert_digit(num)
+
+  return word
